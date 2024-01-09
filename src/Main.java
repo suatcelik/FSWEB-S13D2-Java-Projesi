9060094069d88dc7c7b1d3dc63840bf8f72575ba
suat çelik <sc.suatcelik@gmail.com>
2024-01-09 15:08:32 +0300

    Polindrom sayıyı bulma

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+
+
     }
+
+    public static boolean isPalindrome(int number) {
+        if (number<0) {
+            number= -number;
+        }
+        int temp =number;
+        int rev=0;
+        while (number > 0){
+            int digit = number % 10;
+            rev = rev * 10 + digit;
+            number = number / 10;
+        }
+        return temp == rev;
+    }
+
 }
